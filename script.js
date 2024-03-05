63d742f2e9993efcd50433db47556d41f0a032be
Nurlan <nurlanea@code.edu.az>
2024-03-05 12:35:59 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+/* boyukden kiciye */
+let array = [2,5,1,4,3];
+array.sort(function(a, b){return b - a});
+console.log(array);
+/* boyukden kiciye */
+
+/* herf sayi */
+let words = "The quick brown fox jumps over the lazy dog".split(" ");
+let letterCounts = words.map(word => word.length);
+console.log(letterCounts);
+/* herf sayi */
